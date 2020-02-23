85e12b5f98f3d60718a3fefe0fd02c8345c84eef
queil <queil@users.noreply.github.com>
2020-02-23 21:16:02 +0000

    Added manifests, tasks.json, and suave

--- src/Program.fs
+++ src/Program.fs
@@ -1,8 +1,32 @@
-﻿// Learn more about F# at http://fsharp.org
-
 open System
+open System.Threading
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open System.Net
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main argv = 
+
+    let cts = new CancellationTokenSource()
+    let conf = { defaultConfig with 
+                  cancellationToken = cts.Token
+                  bindings = [HttpBinding.createSimple Protocol.HTTP "0.0.0.0" 8080]
+               }
+
+    let debugHere : WebPart =
+          fun (x : HttpContext) ->
+            async {
+              
+              return! OK "I am up!" x
+            }
+
+    let app = GET >=> debugHere
+
+    let _ , server = startWebServerAsync conf app
+
+    Async.Start(server, cts.Token)
+    Console.ReadKey true |> ignore
+    cts.Cancel()
+    0
